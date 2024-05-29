6633da570eea3a12b3ec9afbd30246a5711a8287
PhanQuanMacM2 <phanquan.2401@gmail.com>
2024-05-29 17:11:22 +0700

    Finish Two Sum practice for Python and Java

--- LeetCodeJava/src/practices/TwoSum.java
+++ LeetCodeJava/src/practices/TwoSum.java
@@ -1,6 +1,8 @@
 package practices;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -24,25 +26,89 @@ public class TwoSum {
 
     /**
      * The brute force approach is simple. Loop through each element xxx and find if there is another value that equals to target−x.
-     *
-     * @param nums   array of numbers
-     * @param target equal to sum of 2 elements in nums array
-     * @return array of index of those 2 elements in the array
+     * Time complexity: O(n^2)
+     * Space complexity: O(1)
      */
     public static int[] bruteForceSolution(final int[] nums, final int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int complement = target - nums[j];
+                if (nums[i]==complement) {
                     return new int[]{i, j};
                 }
             }
         }
+
+        return new int[]{};
+    }
+
+    /**
+     * This solution is about extracting the nested loop and using Map instead, reduce the complexity of the function.
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    public static int[] twoPassHashTableSolution(final int[] nums, final int target) {
+        int n = nums.length;
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        // Not working with IntStream
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement)!=i) {
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+
+        return new int[]{};
+    }
+
+    /**
+     * This solution is the same as above two pass hash table but removing 1 for loop
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    public static int[] onePassHashTableSolution(final int[] nums, final int target) {
+        int n = nums.length;
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
         return new int[]{};
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(TwoSum.bruteForceSolution(new int[]{2, 7, 11, 15}, 9)));
-        System.out.println(Arrays.toString(TwoSum.bruteForceSolution(new int[]{3, 2, 4}, 6)));
-        System.out.println(Arrays.toString(TwoSum.bruteForceSolution(new int[]{3, 3}, 6)));
+        int[] nums1 = new int[]{2, 7, 11, 15};
+        int target1 = 9;
+        int[] nums2 = new int[]{3, 2, 4};
+        int target2 = 6;
+        int[] nums3 = new int[]{3, 3};
+        int target3 = 6;
+
+        System.out.println(Arrays.toString(TwoSum.bruteForceSolution(nums1, target1)));
+        System.out.println(Arrays.toString(TwoSum.bruteForceSolution(nums2, target2)));
+        System.out.println(Arrays.toString(TwoSum.bruteForceSolution(nums3, target3)));
+        System.out.println();
+
+        System.out.println(Arrays.toString(TwoSum.twoPassHashTableSolution(nums1, target1)));
+        System.out.println(Arrays.toString(TwoSum.twoPassHashTableSolution(nums2, target2)));
+        System.out.println(Arrays.toString(TwoSum.twoPassHashTableSolution(nums3, target3)));
+        System.out.println();
+
+        System.out.println(Arrays.toString(TwoSum.onePassHashTableSolution(nums1, target1)));
+        System.out.println(Arrays.toString(TwoSum.onePassHashTableSolution(nums2, target2)));
+        System.out.println(Arrays.toString(TwoSum.onePassHashTableSolution(nums3, target3)));
+        System.out.println();
     }
 }
--- LeetCodePython/src/practices/two_sum.py
+++ LeetCodePython/src/practices/two_sum.py
@@ -1,13 +1,108 @@
 from typing import List
 
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to t
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
 
 class TwoSum:
+    """
+    The brute force approach is simple. Loop through each element xxx and find if there is another value that equals to target−x.
+    Time complexity: O(n^2)
+    Space complexity: O(1)
+    """
 
     @staticmethod
-    def brute_force_solution(self, nums: List[int], target: int) -> List[int]:
+    def brute_force_solution(nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        for i in range(n - 1):
+
+        for i in range(n):
             for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
+                complement = target - nums[j]
+                if nums[i] == complement:
                     return [i, j]
+
         return []
+
+    """
+    This solution is about extracting the nested loop and using Map instead, reduce the complexity of the function.
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+
+    @staticmethod
+    def two_pass_hash_table(nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        num_map = {}
+
+        for i in range(n):
+            num_map[nums[i]] = i
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in num_map and num_map[complement] != i:
+                return [i, num_map[complement]]
+
+        return []
+
+    """
+    This solution is the same as above two pass hash table but removing 1 for loop.
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+
+    @staticmethod
+    def one_pass_hash_table(nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        num_map = {}
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[nums[i]] = i
+
+        return []
+
+
+def main():
+    nums1 = [2, 7, 11, 15]
+    target1 = 9
+    nums2 = [3, 2, 4]
+    target2 = 6
+    nums3 = [3, 3]
+    target3 = 6
+
+    print(TwoSum.brute_force_solution(nums1, target1))
+    print(TwoSum.brute_force_solution(nums2, target2))
+    print(TwoSum.brute_force_solution(nums3, target3))
+    print()
+
+    print(TwoSum.two_pass_hash_table(nums1, target1))
+    print(TwoSum.two_pass_hash_table(nums2, target2))
+    print(TwoSum.two_pass_hash_table(nums3, target3))
+    print()
+
+    print(TwoSum.one_pass_hash_table(nums1, target1))
+    print(TwoSum.one_pass_hash_table(nums2, target2))
+    print(TwoSum.one_pass_hash_table(nums3, target3))
+    print()
+
+
+if __name__ == "__main__":
+    main()
